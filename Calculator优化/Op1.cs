390f9151f2ba87a275ea69a64933c02e1985b453
220511 <72077602+220511@users.noreply.github.com>
2020-10-12 08:54:57 +0800

    Add files via upload

---
+++ Calculator优化/Op1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Op1:Add
+    {
+        public override void Add()
+        {
+            Console.WriteLine("请输入第一个整数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个整数:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("两数之和结果为:{0}", a + b);
+        }
+        public override void Sub()
+        {
+            Console.WriteLine("请输入第一个整数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个整数:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("两数之差结果为:{0}", a - b);
+        }
+        public void Mul()
+        {
+            Console.WriteLine("请输入第一个整数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个整数:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("两数之积结果为:{0}", a * b);
+        }
+        public void Div()
+        {
+            Console.WriteLine("请输入第一个整数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个整数:");
+            int b = int.Parse(Console.ReadLine());
+            if (b != 0)
+                Console.WriteLine("两数相除结果为:{0}", (double)a / b);
+            else
+                Console.WriteLine("除数不能为0！请重新输入！");
+        }
+        public void Equals()
+        {
+            Console.WriteLine("请输入第一个整数:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个整数:");
+            int b = int.Parse(Console.ReadLine());
+            if (a == b)
+                Console.WriteLine("两数相等！");
+            else
+                Console.WriteLine("两数不相等！");
+        }
+    }
+}
---
+++ Calculator优化/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入需要进行的运算符号（+，-，*，/）:");
+            Op1 op1 = new Op1();
+            Op2 op2 = new Op2();
+            string c = Console.ReadLine();
+            if (c == "+")
+            {
+                op1.Add();
+                op1.Equals();
+                op2.Add();
+            }
+            else if (c == "-")
+            {
+                op1.Sub();
+                op1.Equals();
+                op2.Sub();
+            }
+            else if (c == "*")
+            {
+                op1.Mul();
+                op1.Equals();
+            }
+            else if (c == "/")
+            {
+                op1.Div();
+                op1.Equals();
+            }
+        }
+    }
+}
